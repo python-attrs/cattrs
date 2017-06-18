2a3f4ff6686f1630348a73dd62d7ad8e3215dff5
Tin Tvrtkovic <tinchester@gmail.com>
2017-06-18 20:17:09 +0200

    Disable Hypothesis health check for PyPy.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+import platform
+
 import pytest
+
+from hypothesis import HealthCheck, settings
 
 from cattr import Converter
 
@@ -6,3 +10,7 @@ from cattr import Converter
 @pytest.fixture()
 def converter():
     return Converter()
+
+
+if platform.python_implementation() == 'PyPy':
+    settings.default.suppress_health_check.append(HealthCheck.too_slow)
