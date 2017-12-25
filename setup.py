be438d5566bd308b584359a9b0011a7bd0006b06
Tin Tvrtkovic <tintvrtkovic@MonsterMacBook.lan>
2017-12-25 13:52:14 +0100

    0.7.0 open for business.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setup(
     name='cattrs',
-    version='0.6.0',
+    version='0.7.0.dev0',
     description="Composable complex class support for attrs.",
     long_description=readme + '\n\n' + history,
     author="Tin Tvrtković",
