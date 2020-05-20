8c76f4316f987171e531e2bb795095c44930bc20
Darren Meehan <hi@drn.ie>
2020-05-20 08:40:59 +0100

    Simplify logic based on python version

--- src/cattr/_compat.py
+++ src/cattr/_compat.py
@@ -11,14 +11,12 @@ from typing import (
 )
 
 version_info = sys.version_info[0:3]
-is_py37 = version_info[:2] == (3, 7)
-is_py38 = version_info[:2] == (3, 8)
-is_py39 = version_info[:2] == (3, 9)
+is_py36 = version_info[:2] == (3, 6)
 
 unicode = str
 bytes = bytes
 
-if is_py37 or is_py38 or is_py39:
+if not is_py36:
     from typing import List, Union, _GenericAlias
 
     def is_union_type(obj):
--- tests/_compat.py
+++ tests/_compat.py
@@ -1,15 +1,11 @@
-from cattr._compat import is_py37, is_py38, is_bare
+from cattr._compat import is_py36, is_bare
 
-if is_py37 or is_py38:
-
+if is_py36:
+    def change_type_param(cl, new_params):
+        cl.__args__ = (new_params,)
+        return cl
+else:
     def change_type_param(cl, new_params):
         if is_bare(cl):
             return cl[new_params]
         return cl.copy_with(new_params)
-
-
-else:
-
-    def change_type_param(cl, new_params):
-        cl.__args__ = (new_params,)
-        return cl
