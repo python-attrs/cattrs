7a67c3fb9bf96d3641ad65e63ba44bb48f717458
Kaxil Naik <kaxilnaik@gmail.com>
2020-10-30 13:44:02 +0000

    Set Explicit requirement for Python>=3.7
    Check https://packaging.python.org/guides/dropping-older-python-versions/ for more details

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     license="MIT license",
     zip_safe=False,
     keywords="cattrs",
+    python_requires='~=3.7',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
