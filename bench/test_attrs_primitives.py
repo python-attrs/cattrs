3cfb285e0ab91635aeabd4789cab7e2cd769a3ae
Tin Tvrtkovic <tinchester@gmail.com>
2021-02-15 00:28:44 +0100

    New benchmark

--- bench/test_attrs_primitives.py
+++ bench/test_attrs_primitives.py
@@ -6,6 +6,45 @@ import pytest
 from cattr import Converter, GenConverter, UnstructureStrategy
 
 
+class E(IntEnum):
+    ONE = 1
+    TWO = 2
+
+
+@attr.define
+class C:
+    a: int
+    b: float
+    c: str
+    d: bytes
+    e: E
+    f: int
+    g: float
+    h: str
+    i: bytes
+    j: E
+    k: int
+    l: float
+    m: str
+    n: bytes
+    o: E
+    p: int
+    q: float
+    r: str
+    s: bytes
+    t: E
+    u: int
+    v: float
+    w: str
+    x: bytes
+    y: E
+    z: int
+    aa: float
+    ab: str
+    ac: bytes
+    ad: E
+
+
 @pytest.mark.parametrize(
     "converter_cls",
     [Converter, GenConverter],
@@ -18,43 +57,6 @@ def test_unstructure_attrs_primitives(
     benchmark, converter_cls, unstructure_strat
 ):
     """Benchmark a large (30 attributes) attrs class containing primitives."""
-
-    class E(IntEnum):
-        ONE = 1
-        TWO = 2
-
-    @attr.define
-    class C:
-        a: int
-        b: float
-        c: str
-        d: bytes
-        e: E
-        f: int
-        g: float
-        h: str
-        i: bytes
-        j: E
-        k: int
-        l: float
-        m: str
-        n: bytes
-        o: E
-        p: int
-        q: float
-        r: str
-        s: bytes
-        t: E
-        u: int
-        v: float
-        w: str
-        x: bytes
-        y: E
-        z: int
-        aa: float
-        ab: str
-        ac: bytes
-        ad: E
 
     c = converter_cls(unstruct_strat=unstructure_strat)
 
@@ -93,3 +95,56 @@ def test_unstructure_attrs_primitives(
             E.TWO,
         ),
     )
+
+
+@pytest.mark.parametrize(
+    "converter_cls",
+    [Converter, GenConverter],
+)
+@pytest.mark.parametrize(
+    "unstructure_strat",
+    [UnstructureStrategy.AS_DICT, UnstructureStrategy.AS_TUPLE],
+)
+def test_structure_attrs_primitives(
+    benchmark, converter_cls, unstructure_strat
+):
+    """Benchmark a large (30 attributes) attrs class containing primitives."""
+
+    c = converter_cls(unstruct_strat=unstructure_strat)
+
+    inst = C(
+        1,
+        1.0,
+        "a small string",
+        "test".encode(),
+        E.ONE,
+        2,
+        2.0,
+        "a small string",
+        "test".encode(),
+        E.TWO,
+        3,
+        3.0,
+        "a small string",
+        "test".encode(),
+        E.ONE,
+        4,
+        4.0,
+        "a small string",
+        "test".encode(),
+        E.TWO,
+        5,
+        5.0,
+        "a small string",
+        "test".encode(),
+        E.ONE,
+        6,
+        6.0,
+        "a small string",
+        "test".encode(),
+        E.TWO,
+    )
+
+    raw = c.unstructure(inst)
+
+    benchmark(c.structure, raw, C)
