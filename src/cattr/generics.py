112e3090d63229a74bf9758ccc4327e762290e8a
Tin Tvrtkovic <tinchester@gmail.com>
2021-12-04 16:05:09 +0100

    Tweak generics handling

--- src/cattr/generics.py
+++ src/cattr/generics.py
@@ -10,13 +10,13 @@ def deep_copy_with(t, mapping: Mapping[str, Any]):
         # If we're dealing with `Annotated`, we only map the first type parameter
         rest = tuple(args[1:])
         args = (args[0],)
-    return copy_with(
-        t,
+    new_args = (
         tuple(
             mapping[a.__name__]
             if hasattr(a, "__name__") and a.__name__ in mapping
             else (deep_copy_with(a, mapping) if is_generic(a) else a)
             for a in args
         )
-        + rest,
+        + rest
     )
+    return copy_with(t, new_args) if new_args != args else t
