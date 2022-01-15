74665a630297c49e5e0acfaf16213b9f375b3949
Tin Tvrtković <tinchester@gmail.com>
2022-01-15 02:02:45 +0100

    Extended validation first pass

--- src/cattrs/errors.py
+++ src/cattrs/errors.py
@@ -1,3 +1,27 @@
-from cattr.errors import StructureHandlerNotFoundError
+from typing import Dict, Type
 
-__all__ = ["StructureHandlerNotFoundError"]
+from attr import define
+
+
+class StructureHandlerNotFoundError(Exception):
+    """Error raised when structuring cannot find a handler for converting inputs into :attr:`type_`."""
+
+    def __init__(self, message: str, type_: Type) -> None:
+        super().__init__(message)
+        self.type_ = type_
+
+
+class ValidationError(Exception):
+    pass
+
+
+@define
+class IterableValidationError(ValidationError):
+    errors_by_index: Dict[int, Exception]
+
+
+@define
+class ClassValidationError(ValidationError):
+    """Raised when validating a class if any attributes are invalid."""
+
+    errors_by_attribute: Dict[str, Exception]
