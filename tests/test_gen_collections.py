d065e7f88a45ce02209bf4ff0398a5716c95642c
Tin Tvrtković <tinchester@gmail.com>
2024-01-13 19:54:42 +0100

    Some more coverage, update Pendulum

--- tests/test_gen_collections.py
+++ tests/test_gen_collections.py
@@ -1,11 +1,11 @@
 """Tests for collections in `cattrs.gen`."""
-from typing import Generic, Mapping, NewType, TypeVar
+from typing import Generic, Mapping, NewType, Tuple, TypeVar
 
 from cattrs import Converter
-from cattrs.gen import make_mapping_structure_fn
+from cattrs.gen import make_hetero_tuple_unstructure_fn, make_mapping_structure_fn
 
 
-def test_structuring_mappings(converter: Converter):
+def test_structuring_mappings(genconverter: Converter):
     """The `key_type` parameter works for generics with 1 type variable."""
     T = TypeVar("T")
 
@@ -17,8 +17,15 @@ def test_structuring_mappings(converter: Converter):
 
     Key = NewType("Key", str)
 
-    converter.register_structure_hook(Key, key_hook)
+    genconverter.register_structure_hook(Key, key_hook)
 
-    fn = make_mapping_structure_fn(MyMapping[int], converter, key_type=Key)
+    fn = make_mapping_structure_fn(MyMapping[int], genconverter, key_type=Key)
 
     assert fn({"a": 1}, MyMapping[int]) == {"a1": 1}
+
+
+def test_unstructure_hetero_tuple_to_tuple(genconverter: Converter):
+    """`make_hetero_tuple_unstructure_fn` works when unstructuring to tuple."""
+    fn = make_hetero_tuple_unstructure_fn(Tuple[int, str, int], genconverter, tuple)
+
+    assert fn((1, "1", 2)) == (1, "1", 2)
