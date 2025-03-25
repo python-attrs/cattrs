11e28f4330cd969780c8b14ac00b6e2fd0498173
Tin Tvrtković <tinchester@gmail.com>
2025-03-25 15:48:19 +0100

    Dataclasses: fix kwonly arg recognition (#638)

--- tests/test_dataclasses.py
+++ tests/test_dataclasses.py
@@ -2,8 +2,11 @@ import dataclasses
 from typing import List
 
 import attr
+import pytest
 
 from cattrs import BaseConverter
+
+from ._compat import is_py310_plus
 
 
 @dataclasses.dataclass
@@ -41,3 +44,20 @@ def test_dataclasses(converter: BaseConverter):
 
     assert converter.unstructure(struct) == unstruct
     assert converter.structure(unstruct, Foo) == struct
+
+
+@pytest.mark.skipif(not is_py310_plus, reason="kwonly fields are Python 3.10+")
+def test_kw_only_propagation(converter: BaseConverter):
+    """KW-only args work.
+
+    Reproducer from https://github.com/python-attrs/cattrs/issues/637.
+    """
+
+    @dataclasses.dataclass
+    class PartialKeywords:
+        a1: str = "Default"
+        a2: str = dataclasses.field(kw_only=True)
+
+    assert converter.structure({"a2": "Value"}, PartialKeywords) == PartialKeywords(
+        a1="Default", a2="Value"
+    )
