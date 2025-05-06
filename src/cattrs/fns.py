3de6772f9fde1423c8a95e287d6e9c5a44802233
Michael.Makukha <m.makukha@gmail.com>
2025-05-06 18:52:10 +0300

    Add utility function

--- src/cattrs/fns.py
+++ src/cattrs/fns.py
@@ -1,9 +1,11 @@
 """Useful internal functions."""
 
+from functools import wraps
 from typing import Any, Callable, NoReturn, TypeVar
 
 from ._compat import TypeAlias
 from .errors import StructureHandlerNotFoundError
+from .types import StructuredValue, StructureHook, TargetType, UnstructuredValue
 
 T = TypeVar("T")
 
@@ -14,6 +16,14 @@ Predicate: TypeAlias = Callable[[Any], bool]
 def identity(obj: T) -> T:
     """The identity function."""
     return obj
+
+
+def bypass(target: type, structure_hook: StructureHook) -> StructureHook:
+    """Bypass structure hook when given object of target type."""
+    @wraps(structure_hook)
+    def wrapper(obj: UnstructuredValue, cl: TargetType) -> StructuredValue:
+        return obj if type(obj) is target else structure_hook(obj, cl)
+    return wrapper
 
 
 def raise_error(_, cl: Any) -> NoReturn:
