5870243511cf5914d0125f88b1da9c10b472b6d0
Tin Tvrtkovic <tinchester@gmail.com>
2025-06-28 17:20:34 +0200

    Improve coverage

--- tests/test_generics.py
+++ tests/test_generics.py
@@ -1,4 +1,5 @@
 from collections import deque
+from collections.abc import Sequence
 from typing import Deque, Dict, Generic, List, Optional, TypeVar, Union
 
 import pytest
@@ -109,9 +110,16 @@ def test_structure_nested_generics_with_cols(t, result, genconverter: Converter)
     (
         (TClass[int, int, int], str, int, TClass(TClass(1, 2), "a")),
         (List[TClass[int, int, int]], str, int, TClass([TClass(1, 2)], "a")),
+        (
+            Sequence[TClass[str, str, str]],
+            str,
+            str,
+            TClass((TClass("a", "b", "c"),), "b", "c"),
+        ),
     ),
 )
 def test_structure_nested_generics(converter: BaseConverter, t, t2, t3, result):
+    """Structuring nested generics works."""
     res = converter.structure(asdict(result), TClass[t, t2, t3])
 
     assert res == result
@@ -161,7 +169,7 @@ def test_structure_deque_of_generic_unions(converter):
 
 
 def test_raises_if_no_generic_params_supplied(
-    converter: Union[Converter, BaseConverter]
+    converter: Union[Converter, BaseConverter],
 ):
     data = TClass(1, "a")
 
