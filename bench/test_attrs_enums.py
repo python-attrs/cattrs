6aba3d46e452f9b2a13c700d04a1db9c263af5c2
Tin Tvrtkovic <tinchester@gmail.com>
2025-12-14 22:58:55 +0100

    Focus the benchmarks

--- bench/test_attrs_enums.py
+++ bench/test_attrs_enums.py
@@ -1,9 +1,8 @@
 from enum import Enum, IntEnum
 
 import pytest
-from attrs import define
 
-from cattr import BaseConverter, Converter
+from cattrs import BaseConverter, Converter
 
 
 class SimpleEnum(Enum):
@@ -16,12 +15,6 @@ class SimpleIntEnum(IntEnum):
     ONE = 1
     TWO = 2
     THREE = 3
-
-
-@define
-class EnumContainer:
-    simple_enum: SimpleEnum
-    simple_int_enum: SimpleIntEnum
 
 
 @pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
@@ -50,19 +43,3 @@ def test_structure_simple_int_enum(benchmark, converter_cls):
     """Benchmark structuring a simple IntEnum."""
     c = converter_cls()
     benchmark(c.structure, 1, SimpleIntEnum)
-
-
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
-def test_unstructure_attrs_enum_container(benchmark, converter_cls):
-    """Benchmark unstructuring an attrs class containing enums."""
-    c = converter_cls()
-    instance = EnumContainer(simple_enum=SimpleEnum.SECOND, simple_int_enum=SimpleIntEnum.TWO)
-    benchmark(c.unstructure, instance)
-
-
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
-def test_structure_attrs_enum_container(benchmark, converter_cls):
-    """Benchmark structuring an attrs class containing enums."""
-    c = converter_cls()
-    data = {"simple_enum": "second", "simple_int_enum": 2}
-    benchmark(c.structure, data, EnumContainer)
