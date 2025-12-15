397e1d4543fe9047bb19ce6b9b50a220b71a95ff
Tin Tvrtkovic <tinchester@gmail.com>
2025-12-15 22:13:43 +0100

    feat: Bench enums on msgspec and orjson preconf converters
    The  benchmark is removed since it's now
    redundant.

--- bench/test_enums.py
+++ bench/test_enums.py
@@ -3,6 +3,8 @@ from enum import Enum, IntEnum, StrEnum
 import pytest
 
 from cattrs import BaseConverter, Converter
+from cattrs.preconf.msgspec import MsgspecJsonConverter
+from cattrs.preconf.orjson import OrjsonConverter
 
 
 class SimpleEnum(Enum):
@@ -23,42 +25,42 @@ class SimpleStrEnum(StrEnum):
     GAMMA = "gamma"
 
 
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
+@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter, MsgspecJsonConverter, OrjsonConverter])
 def test_unstructure_simple_enum(benchmark, converter_cls):
     """Benchmark unstructuring a simple enum."""
     c = converter_cls()
     benchmark(c.unstructure, SimpleEnum.FIRST)
 
 
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
+@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter, MsgspecJsonConverter, OrjsonConverter])
 def test_structure_simple_enum(benchmark, converter_cls):
     """Benchmark structuring a simple enum."""
     c = converter_cls()
     benchmark(c.structure, "first", SimpleEnum)
 
 
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
+@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter, MsgspecJsonConverter, OrjsonConverter])
 def test_unstructure_simple_int_enum(benchmark, converter_cls):
     """Benchmark unstructuring a simple IntEnum."""
     c = converter_cls()
     benchmark(c.unstructure, SimpleIntEnum.ONE)
 
 
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
+@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter, MsgspecJsonConverter, OrjsonConverter])
 def test_structure_simple_int_enum(benchmark, converter_cls):
     """Benchmark structuring a simple IntEnum."""
     c = converter_cls()
     benchmark(c.structure, 1, SimpleIntEnum)
 
 
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
+@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter, MsgspecJsonConverter, OrjsonConverter])
 def test_unstructure_simple_str_enum(benchmark, converter_cls):
     """Benchmark unstructuring a simple StrEnum."""
     c = converter_cls()
     benchmark(c.unstructure, SimpleStrEnum.ALPHA)
 
 
-@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter])
+@pytest.mark.parametrize("converter_cls", [BaseConverter, Converter, MsgspecJsonConverter, OrjsonConverter])
 def test_structure_simple_str_enum(benchmark, converter_cls):
     """Benchmark structuring a simple StrEnum."""
     c = converter_cls()
